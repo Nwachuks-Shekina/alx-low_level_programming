4d4125b6491d28194aaeb4fbf857c94dec25bf6c
Nwachukwu Shekina <nwachukwushekina@gmail.com>
2022-04-07 15:01:14 -0700

    Quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
+ * main - Function begins
+ * Return: Function ends
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
