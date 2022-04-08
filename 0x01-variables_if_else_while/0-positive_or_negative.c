363c1030c07bcc2df8b8a9dece115b2ea1ca87bf
Nwachukwu Shekina <nwachukwushekina@gmail.com>
2022-04-08 08:16:41 -0700

    Positivetive or Negative c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,26 +3,30 @@
 #include <stdio.h>
 
 /*
- * main - Prints a random number and states whether
- * 	   it is postive, negative, or zero.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+srand(time(0)) ;
+n = rand() - RAND_MAX / 2 ;
 
-if (n > 1)
-printf("%d is positive\n" , n);
+if (n == 0)
+{
+printf("%i is zero\n" , n) ;
+}
 else if (n < 0)
-printf("%d is negative\n" , n);
+{
+printf("%i is negative\n" , n) ;
+}
 else
+{
+printf("%i is positive\n" , n) ;
+}
 
-printf("%d is zero\n" , n);
-
-return (0);
+return (0) ;
 }
 
