b95ecd67cfa689fe39469280e900645974a39631
Nwachukwu Shekina <nwachukwushekina@gmail.com>
2022-04-09 12:58:43 -0700

    PrintComb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints the numbers from 00 to 99
+ * main - Prints all possible different combinations of two digits
  *
  * Return: Always (Success)
  */
 int main(void)
 {
-	int c, i;
+	int c, f_d, l_d, c2, f_d2, l_d2;
 
-	c = i = '0';
+	c = '0';
 
-	for (c = '0'; c <= '9'; c++)
+	while (c <= 98)
 	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			putchar(c);
-			putchar(i);
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
 
-			if ((c != '9') || (c == '9' && i != '9'))
+		while (c2 <= 99)
+		{
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
 			{
-				putchar(',');
+				putchar(f_d);
+				putchar(l_d);
 				putchar(' ');
+				putchar(f_d2);
+				putchar(l_d2);
+
+				if (c != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			c2++;
 		}
+		c++;
 	}
 
 	putchar('\n');
