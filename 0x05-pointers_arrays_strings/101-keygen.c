137480e50adeb224bf6d0dca31faadf2798c9470
Shekina Nwachukwu <43779317+Nwachuks-Shekina@users.noreply.github.com>
2022-04-20 01:27:56 -0700

    KeyGen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main – program to generate random valid passwords
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char x[300];
+int num = 0;
+int randomizer = 0;
+char *key = x;
+
+srand(time(NULL));
+
+while (num < 2650)
+{
+randomizer = rand() % 122;
+
+if (randomizer > 32)
+{
+*key = randomizer;
+key = key + 1;
+num += randomizer;
+}
+}
+
+*key = (2772 - num);
+*(key + 1) = '\n';
+printf("%s", x);
+return (0);
+}
