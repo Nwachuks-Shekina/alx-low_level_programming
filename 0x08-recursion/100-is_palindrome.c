904a86fccfea50978cdc368b386f4a84f28537c5
Shekina Nwachukwu <43779317+Nwachuks-Shekina@users.noreply.github.com>
2022-04-28 03:50:55 -0700

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,46 @@
 #include "main.h"
+/**
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: string
- * Return: 1 if string is palindrome, 0 if not
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int is_palindrome(char *s)
 {
-int length;
-length = get_length(s) - 1;
-return (my_pal(s, --length));
-}
+	int l;
 
-/**
- * get_length - determines length of a string
- * @s: string
- * Return: length of string
- */
-
-int get_length(char *s)
-{
-if (*s == '\0')
-return (1);
-else
-return (1 + get_length(++s));
-}
-
-/**
- * my_pal - recursive check of palindrome
- * @s: string
- * @l: length of string
- * Return: 1 if string is a palindrome, 0 if not
- */
-
-int my_pal(char *s, int l)
-{
-if (*s == *(s + l))
-{
-if (l <= 0)
-return (1);
-else
-return (my_pal(++s, l - 2));
-}
-else
-return (0);
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
